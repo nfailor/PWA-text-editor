18927775595f791cf2106cda0f295a4e62171716
nfailor <nfailor@gmail.com>
2024-01-28 19:04:48 -0600

    Updated put request in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add({ content });
+  await tx.done;
+  console.log('Content added to the database');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 
